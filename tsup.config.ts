37718e9e15ab0c8fe9cc1d3808bab72acb41ce82
Jack Baron <jackmbaron@gmail.com>
2022-09-22 04:05:46 +0100

    cool

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, type Options } from 'tsup'
 
 export function createTsupConfig({
   entry = ['./src/index.ts'],
   external = [],
   noExternal = [],
   platform = 'node',
-  format = ['esm', 'cjs'],
+  format = 'esm',
   target = 'es2022',
   skipNodeModulesBundle = true,
   clean = true,
@@ -15,7 +15,7 @@ export function createTsupConfig({
   keepNames = true,
   dts = true,
   sourcemap = true,
-} = {}) {
+}: Options = {}) {
   return defineConfig({
     entry,
     external,
@@ -34,4 +34,4 @@ export function createTsupConfig({
   })
 }
 
-export default createTsupConfig()
+export default createTsupConfig({ format: ['esm', 'cjs'] })
